514bb943017acca0f30824eaf668e134ef30251c
kupal-coder <darksoulsfakev2@gmail.com>
2026-08-19 09:29:05 +0800

    Update Hazard.hpp

--- gd-sim/include/Hazard.hpp
+++ gd-sim/include/Hazard.hpp
@@ -1,12 +1,22 @@
 #pragma once
+
 #include <Object.hpp>
+#include <unordered_map>
+#include <string>
+
+struct Player;
 
 struct Hazard : public Object {
     Hazard(Vec2D size, std::unordered_map<int, std::string>&& fields);
-    void collide(Player&) const override;
+    void collide(Player& player) const override;
 };
 
 struct Sawblade : public Hazard {
     using Hazard::Hazard;
-    bool touching(Player const&) const override;
+    bool touching(Player const& player) const override;
+};
+
+struct Spike : public Hazard {
+    using Hazard::Hazard;
+    bool touching(Player const& player) const override;
 };
